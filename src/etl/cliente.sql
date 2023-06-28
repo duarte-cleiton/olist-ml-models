491d9c102ee2c19a5a0d60cfa2c7dc856dae25bb
duarte.cleiton@outlook.com <duarte.cleiton@outlook.com>
2023-06-28 10:48:32 +0000

    Join para calcular estados de venda

---
+++ src/etl/cliente.sql
@@ -0,0 +1,24 @@
+-- Databricks notebook source
+with tb_join as (
+  select
+    distinct
+    t1.idPedido,
+    t1.idCliente,
+    t2.idVendedor,
+    t3.descUF
+
+  from silver.olist.pedido as t1
+
+  left join silver.olist.item_pedido as t2
+  on t1.idPedido = t2.idPedido
+
+  left join silver.olist.cliente as t3
+  on t1.idCliente = t3.idCliente
+
+  where t1.dtPedido <= '2018-01-01'
+  and t1.dtPedido >= add_months('2018-01-01', -6)
+)
+
+select * from tb_join
+;
+
